422ce5afc625a11350eb5aaf36d84728300f08ba
Kapil Sahu <116225142+DevKapil-101@users.noreply.github.com>
2023-08-04 21:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 function palindrome(s){
 let reversedString = "";
   for (let i = s.length - 1; i >= 0; i--) {
-    reversedString += s[i];
+    reversedString += s[i].toLowerCase();
   }
 
-  // Compare the reversed string to the original string.
-  return s === reversedString;
+  // Compare the reversed string to the original string, ignoring case.
+  return s.toLowerCase() === reversedString;
 }
 module.exports = palindrome
