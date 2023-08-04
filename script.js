0834aa50b409ef2ca5ebac9ed31f244555a2a626
Kapil Sahu <116225142+DevKapil-101@users.noreply.github.com>
2023-08-04 21:24:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // complete the given function
 
-function palindrome(str){
-const rStr = str.reverse();
-	if (rStr === str) return true;
-	else return false;
+function palindrome(s){
+let reversedString = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    reversedString += s[i];
+  }
+
+  // Compare the reversed string to the original string.
+  return s === reversedString;
 }
 module.exports = palindrome
